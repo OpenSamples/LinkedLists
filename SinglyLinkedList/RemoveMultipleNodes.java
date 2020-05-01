b7e99df64bc4032a40a602e4bd731f22c763eee1
ivanab <ivana@opticaltone.com>
2020-05-01 15:47:32 +0200

    Added method for adding nodes at the beginning of the list

--- SinglyLinkedList/RemoveMultipleNodes.java
+++ SinglyLinkedList/RemoveMultipleNodes.java
@@ -1,8 +1,24 @@
 public class RemoveMultipleNodes {
   
   public static void main(String[] args) {
+    
     CharList list = new CharList();
+    
+    System.out.println("Enter how many chars you want in the list:");
+    int n = Svetovid.in.readInt();
+    
+    System.out.println("Enter characters:");
+    
+    for (int i = 0; i < n; i++) {
+      char ch = Svetovid.in.readChar();
+      list.addAtBeginning(ch);
+    }
+    
+    System.out.println("Complete list " + list.toString());
+    
     list.removeAllLowerCaseLetters();
+    
+    System.out.println("List without lower case letters: " + list.toString());
   }
 }
 
@@ -29,6 +45,12 @@ class CharList {
     first = null;
   } 
   
+  public void addAtBeginning(char ch) {
+    Node newNode = new Node(ch);
+    newNode.next = first;
+    first = newNode;
+  }
+  
   public int removeAllLowerCaseLetters() {
     int counter = 0;
     // remove characters from beginning of the list
@@ -53,6 +75,22 @@ class CharList {
       }
     }
     return counter;
+  }
+  
+  public String toString() {
+    String result = "[ ";
+    Node current = first;
+    while (current != null) {
+      if (current.next != null) {
+        result += current.character + ", ";
+      }
+      else {
+        result += current.character + " ";
+      }
+      current = current.next;
+    }
+    result += "]";
+    return result;
   }
 }
 
