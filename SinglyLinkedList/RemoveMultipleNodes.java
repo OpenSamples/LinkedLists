beac040d92e972985403f8bb61a8c9e6634b5a15
ivanab <ivana@opticaltone.com>
2020-05-01 16:42:25 +0200

    Added method which separates digits while keeping original order

--- SinglyLinkedList/RemoveMultipleNodes.java
+++ SinglyLinkedList/RemoveMultipleNodes.java
@@ -8,17 +8,19 @@ public class RemoveMultipleNodes {
     int n = Svetovid.in.readInt();
     
     System.out.println("Enter characters:");
-    
     for (int i = 0; i < n; i++) {
       char ch = Svetovid.in.readChar();
       list.addAtBeginning(ch);
     }
+    System.out.println("Complete list: " + list.toString());
     
-    System.out.println("Complete list " + list.toString());
+    System.out.println("Number of lower case letters in list is: " 
+                         + list.removeAllLowerCaseLetters()
+                         + "\nList without lower case letters: " 
+                         + list.toString());
     
-    list.removeAllLowerCaseLetters();
-    
-    System.out.println("List without lower case letters: " + list.toString());
+    System.out.println("New list which contains only digits: " 
+                         + list.separateDigitsOrderAsEntered(new CharList()).toString());
   }
 }
 
@@ -26,16 +28,16 @@ class CharList {
   
   class Node {
     
-    char character;
+    char data;
     Node next;
     
     public Node(char ch) {
-      character = ch;
+      data = ch;
       next = null;
     }
     
     public String toString() {
-      return character + "";
+      return data + "";
     }
   }
   // pointer to first node
@@ -53,8 +55,8 @@ class CharList {
   
   public int removeAllLowerCaseLetters() {
     int counter = 0;
-    // remove characters from beginning of the list
-    while (first != null && first.character >= 'a' && first.character <= 'z') {
+    // remove datas from beginning of the list
+    while (first != null && first.data >= 'a' && first.data <= 'z') {
       first = first.next;
       counter++;
     }
@@ -65,7 +67,7 @@ class CharList {
         // moving step by step until last node
         Node previous = current;
         current = current.next;
-        if (current.character >= 'a' && current.character <= 'z') {
+        if (current.data >= 'a' && current.data <= 'z') {
           // moving pointers around the node
           previous.next = current.next;
           counter++;
@@ -77,15 +79,55 @@ class CharList {
     return counter;
   }
   
+  public CharList separateDigitsOrderAsEntered(CharList digitsList) {
+    // to be able to add digits at the end of list local variable is required
+    Node digitsEnd = null;
+    Node current, previous;
+    while (first != null && first.data >= '0' && first.data <= '9') {
+      current = first;
+      first = first.next;
+      if (digitsList.first == null) {
+        digitsList.first = current;
+        digitsEnd = current;
+        current.next = null;
+      } else {
+        digitsEnd.next = current;
+        current.next = null;
+        digitsEnd = current;
+      }
+    }
+    if (first != null) {
+      current = first;
+      while (current.next != null) {
+        previous = current;
+        current = current.next;
+        if (current.data >= '0' && current.data <= '9') {
+          previous.next = current.next;
+          if (digitsList.first == null) {
+            digitsList.first = current;
+            current.next = null;
+            digitsEnd = current;
+          } else {
+            digitsEnd.next = current;
+            current.next = null;
+            digitsEnd = current;
+          }
+          current = previous;
+        }
+      }
+    }
+    return digitsList;
+  }
+  
   public String toString() {
     String result = "[ ";
     Node current = first;
     while (current != null) {
       if (current.next != null) {
-        result += current.character + ", ";
+        result += current.data + ", ";
       }
       else {
-        result += current.character + " ";
+        result += current.data + " ";
       }
       current = current.next;
     }
